5c1b41fa336b0e7550f4e45985f3cd5de2c2d837
Wisani Mathevula <158144835+WisaniMawisi@users.noreply.github.com>
2026-08-21 13:23:37 +0200

    Add regex support for CORS origins

--- backend/server.py
+++ backend/server.py
@@ -834,6 +834,7 @@ if not cors_origins:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=cors_origins,
+    allow_origin_regex=r"http://localhost:\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
